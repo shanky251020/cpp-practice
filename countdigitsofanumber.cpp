6e87f0e6d1bac0ef04bede7c8c0e2869c24f9b05
shanky251020 <shashank16septd@gmail.com>
2021-06-08 23:08:22 +0530

    Added Count digits of a number program

---
+++ countdigitsofanumber.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int count =0;
+    int n ;
+    cin>>n;
+    while(n>0)
+    {
+        n=n/10;
+        count++;
+    }
+    cout<<count;
+    return 0;
+}
