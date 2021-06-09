157770ab0918d04159f77ce86f29b3eb64d364b2
shanky251020 <shashank16septd@gmail.com>
2021-06-09 22:15:44 +0530

    Added sum of an array code

---
+++ averageofanarray.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    int average;
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        sum+=arr[i];
+    }
+    average=sum/n;
+    cout<<average;
+}
